34c544dc8aa9077d23282339fb802d6850a419b0
briannatran <brianna.t.tran@gmail.com>
2018-07-25 10:09:43 -0700

    welcome page

---
+++ welcome.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var txt;
+    var person = prompt("Please enter your name:", "");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Welcome " + person ;
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
