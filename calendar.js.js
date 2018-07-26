1dbe2d159b3251f18c524c2779313493b94f0aca
tanaejah321 <tanaejah.reed@gmail.com>
2018-07-26 10:29:33 -0700

    calendar new

---
+++ calendar.js.js
@@ -0,0 +1,4 @@
+
+// var quotes=["We encounter many defeats but, we must not be defeated.-By Maya Angelou","If you always try to be normal you will never know how amazing you can be"];
+// var item = quotes[Math.floor(Math.random()*quotes.length)];
+// document.getElementById("daily").innerHTML = item;
