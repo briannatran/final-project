58eed7d79036d4820c9666058e91408eb4b55513
michymochy <mich3ll3.lai@gmail.com>
2018-07-30 11:05:03 -0700

    fixed the button gradients

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+// Mission Statement
 var modal = document.querySelector(".modal");
     var trigger = document.querySelector(".trigger");
     var closeButton = document.querySelector(".close-button");
@@ -6,8 +7,8 @@ var modal = document.querySelector(".modal");
         modal.classList.toggle("show-modal");
     }
 
-    function windowOnClick(event) {
-        if (event.target === modal) {
+    function windowOnClick(event1) {
+        if (event1.target === modal) {
             toggleModal();
         }
     }
@@ -15,4 +16,22 @@ var modal = document.querySelector(".modal");
     trigger.addEventListener("click", toggleModal);
     closeButton.addEventListener("click", toggleModal);
     window.addEventListener("click", windowOnClick);
-	
+
+// Founders
+var modal2 = document.querySelector(".modal2");
+    var trigger2 = document.querySelector(".trigger2");
+    var closeButton2 = document.querySelector(".close-button2");
+
+    function toggleModal2() {
+        modal2.classList.toggle("show-modal2");
+    }
+
+    function windowOnClick2(event2) {
+        if (event2.target === modal2) {
+            toggleModal2();
+        }
+    }
+
+    trigger2.addEventListener("click", toggleModal2);
+    closeButton2.addEventListener("click", toggleModal2);
+    window.addEventListener("click", windowOnClick2);
